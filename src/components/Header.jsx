54943678461baeee3d47f40ecf440c26d204a8b1
Konain1 <kaunainkasab@gmail.com>
2024-04-01 20:44:44 +0530

    tailwind website

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-[#795FA1]'>
+        <div className='max-w-[1240px] mx-auto border border-black bg-[white] ' > hello</div>
+    </div>
+  )
+}
+
+export default Header
